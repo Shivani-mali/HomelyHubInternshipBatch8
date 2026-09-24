a99396cd562b3720aaeb7058dc042f91055b9d05
Shivani-mali <shivanimali339@gmail.com>
2026-09-25 00:00:02 +0530

    Fix propertyRouter import error for getProperty

--- backend/src/routes/propertyRouter.js
+++ backend/src/routes/propertyRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getProperties, getPropertyById} from "../controllers/propertyController.js";
+import { getProperties, getProperty } from "../controllers/propertyController.js";
 
 const propertyRouter = express.Router();
 
 propertyRouter.route("/").get(getProperties);
-propertyRouter.route("/:id").get(getPropertyById);
+propertyRouter.route("/:id").get(getProperty);
 
-export { propertyRouter };
+export { propertyRouter };
